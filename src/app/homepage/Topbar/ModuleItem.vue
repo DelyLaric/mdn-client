b21cf157669fa4e9d174f6b0a6ab9567c7db6ba2
zhanglan <delylaric@gmail.com>
2018-08-20 13:06:47 +0800

    basic: create homepage view

---
+++ src/app/homepage/Topbar/ModuleItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <p
+    class="level-item is-unselectable"
+    @click="$router.push({name: route})">
+    <strong v-if="hasMatched(route)">流程区域管理</strong>
+    <a v-else>流程区域管理</a>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    route: String,
+
+    text: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    hasMatched (routeName) {
+      return Boolean(this.$route.matched.find(
+        route => route.name === routeName
+      ))
+    }
+  }
+}
+</script>
--- src/app/routes.js
+++ src/app/routes.js
@@ -3,5 +3,13 @@ export default [
     path: '/test',
     name: 'Test',
     component: () => import('./common/test')
+  },
+  {
+    path: '/',
+    name: 'home page',
+    component: () => import('./homepage'),
+    children: [
+
+    ]
   }
 ]
