6449b3c82f48ce878e5d75d0b72c4cbe7252dd10
zhanglan <delylaric@gmail.com>
2018-08-22 04:30:59 +0800

    chore: homepage works

--- src/app/homepage/index.vue
+++ src/app/homepage/index.vue
@@ -1,25 +1,29 @@
 <template>
-  <div class="full-container">
-    <div class="full-container is-flex is-flex-column">
-      <Topbar />
-      <div class="full-container is-flex-auto">
-        <router-view />
+  <DataLoader>
+    <div class="full-container">
+      <div class="full-container is-flex is-flex-column">
+        <Topbar />
+        <div class="full-container is-flex-auto">
+          <router-view />
+        </div>
       </div>
     </div>
-  </div>
+  </DataLoader>
 </template>
 
 <script>
-import Topbar from './Topbar'
-import Configer from './Configer'
+import Topbar from './topbar'
+import Bootstrap from './boot'
+import DataLoader from './data-loader'
 
 export default {
   name: 'Homepage',
 
-  mixins: [Configer],
+  mixins: [Bootstrap],
 
   components: {
-    Topbar
+    Topbar,
+    DataLoader
   }
 }
 </script>
