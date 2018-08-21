3d7d2dde4df94f0ba3228e73877a00a3c458a6bc
zhanglan <delylaric@gmail.com>
2018-08-21 20:40:16 +0800

    feat: add global helpers: waiting && loading

---
+++ src/app/common/waiting.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-show="show" class="modal is-active" style="z-index: 99999">
+    <div class="modal-background"></div>
+    <div class="loading"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    show () {
+      return this.$store.state.isWaiting
+    }
+  }
+}
+</script>
