9ca69a5291f631bf39950a57e5ec37cec2936ffd
zhanglan <delylaric@gmail.com>
2018-08-22 04:30:00 +0800

    fix: use const value to control axios url

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'
 import Axios from 'axios'
 
 const dev = true
 const LOCAL_URL = 'http://mdn.api'
 const SERVER_URL = 'http://115.159.51.252:8000'
 
-if (!Vue.config.productionTip && dev) {
+if (dev) {
   Axios.defaults.baseURL = LOCAL_URL
 } else {
   Axios.defaults.baseURL = SERVER_URL
