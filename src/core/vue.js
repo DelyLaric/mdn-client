14a1321367ea65e7e67cc54cde9259ed8ee3dab5
zhanglan <delylaric@gmail.com>
2018-08-24 16:18:09 +0800

    tech: set rootState.app of vuex

--- src/core/vue.js
+++ src/core/vue.js
@@ -4,13 +4,18 @@ import store from './vuex'
 import bulmaComponents from '@/packages/bulma/components'
 import '@/assets/sass/index.sass'
 import App from '@/app'
+import http from './axios'
 
 Vue.use(bulmaComponents)
 
 Vue.config.productionTip = false
 
-new Vue({
+Vue.prototype.$http = http
+
+const app = new Vue({
   render: h => h(App),
   router,
   store
 }).$mount('#app')
+
+store.state.app = app
