8aaf2ba967308d986e27a7b1b09e29c57fa0e3cb
zhanglan <delylaric@gmail.com>
2018-09-04 11:13:15 +0800

    交换任务视图中备注和任务状态的顺序 && 移除表格的table-item组件重构代码

--- src/app/plant-project-tasks/primary/index.vue
+++ src/app/plant-project-tasks/primary/index.vue
@@ -2,37 +2,74 @@
   <table class="table is-bordered is-nowrapped">
     <thead>
       <th class="is-centered" style="width: 1px">#</th>
-      <th class="is-centered" style="width: 120px;">任务状态</th>
       <th>备注</th>
+      <th class="is-centered" style="width: 120px;">任务状态</th>
       <th class="is-centered" >截止日期</th>
       <th class="is-centered" >创建时间</th>
       <th class="is-centered">操作</th>
     </thead>
     <tbody>
-      <InfoItem
-        v-for="(task, index) in tasks"
-        :total="tasks.length"
-        :index="index"
-        :key="task.id"
-        :task="task"
-        ref="items"
-      />
+      <tr v-for="(task, index) in tasks" :key="task.id">
+        <td class="is-centered">{{index}}</td>
+        <EditableCell
+          :value="task.comment"
+          @change="$wait(() => updateComment({id: task.id, comment: $event}))"
+          ref="comment"
+        />
+        <StatusCell
+          :id="task.id"
+          :index="index"
+          :value="task.status"
+          :total="tasks.length"
+        />
+        <DuetimeCell
+          :id="task.id"
+          :value="task.duetime"
+        />
+        <td class="is-centered">{{task.created_at.slice(0, 10)}}</td>
+        <td class="is-centered">
+          <a @click="handleClickDestroy">
+            删除
+          </a>
+        </td>
+      </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-import InfoItem from './row'
+import { mapActions } from 'vuex'
+import StatusCell from './status-cell'
+import DuetimeCell from './duetime-cell'
+import EditableCell from '@/components/common/editable-cell'
 
 export default {
   name: 'TaskPrimaryView',
 
   components: {
-    InfoItem
+    StatusCell,
+    DuetimeCell,
+    EditableCell
   },
 
   props: {
     tasks: Array
+  },
+
+  methods: {
+    ...mapActions('tasks', [
+      'updateComment'
+    ]),
+
+    handleClickDestroy () {
+      this.$router.push({
+        name: 'task destroy',
+        params: {
+          taskId: this.task.id,
+          ...this.$route.params
+        }
+      })
+    }
   }
 }
 </script>
